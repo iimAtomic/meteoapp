ea6b922aa32c8b0be1e7a99cb9127f0a30c5bb67
iimAtomic <vegbahermerite@gmail.com>
2023-12-09 16:13:46 +0100

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,7 @@ const WeatherForecast = () => {
  const [selectedCity, setSelectedCity] = useState('Paris');
 
  return (
-    <div className="forecast-container">
-      <h1>Météo France</h1>
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Rechercher une ville, un pays.."
-          onChange={(e) => setSelectedCity(e.target.value)}
-        />
-      </div>
-      <h2>{selectedCity}</h2>
-      {/* Add the remaining content of the forecast */}
-    </div>
+   
  );
 };
 
